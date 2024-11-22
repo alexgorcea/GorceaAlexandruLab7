d42df17284ef9f4833caf2fe4c52e05bb4ea008a
Alex Gorcea <alexandru.gorcea1@stud.ubbcluj.ro>
2024-11-22 16:45:19 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace GorceaAlexandruLab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GorceaAlexandruLab7
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
